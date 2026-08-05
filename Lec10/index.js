27247c8ffb74b04a653e26a8e200fb3d38dfbebb
Sumit Kumar <sumitknic2006@gmail.com>
2026-08-05 22:41:39 +0530

    index

---
+++ Lec10/index.js
@@ -0,0 +1,45 @@
+// 1. function ko variable me store ker sakte hai
+// function foo(){
+
+// }
+// console.log(fun);
+
+// // fun() // error : fun is not a function
+// // function expression
+// var fun = function(){
+//     console.log("hello")
+// }
+
+// fun();
+
+// function fun(a,b){
+//     console.log(a+b);
+// }
+
+// function foo(a){
+//     console.log(a);
+    // if a is a function
+    // then a(2,3);
+// }
+
+// foo(function(){}); // Annonymous function
+
+// foo(fun());
+// let result = foo(10);
+// console.log(result);
+// result(2,3);
+
+
+// function foo(cb){
+//     return cb;
+// }
+
+// function fun(){
+//     console.log("hello world");
+// }
+
+// // let result = foo(fun);
+// let result = foo(fun());
+// console.log(result)
+
+
